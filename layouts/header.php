d0ea003bd81b412cf38a811a0fd2100069f096ad
Rafael Lopes <rafaellopes.dev@gmail.com>
2026-03-13 09:37:12 -0300

    fix: cover full page with blur overlay

--- layouts/header.php
+++ layouts/header.php
@@ -7,7 +7,7 @@
   <title>eCommerce CMS</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
   <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous">
-  <link href="assets/css/style.css?v=4" rel="stylesheet">
+  <link href="assets/css/style.css?v=5" rel="stylesheet">
 </head>
 <body>
   <nav class="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
